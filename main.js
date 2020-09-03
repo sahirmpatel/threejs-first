43ebb6fd7fd1897528011ab8e5aaa53f05718d69
Sahir Patel <sahirmpatel@gmail.com>
2020-09-03 12:38:37 +0530

    adding cube

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+//adding a geometric cube
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//mesh takes in the geometry and the matereial
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+animate = () => {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
