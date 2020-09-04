d591a15d8a78afbff90317523c4748d093e65323
Sahir Patel <sahirmpatel@gmail.com>
2020-09-04 16:56:07 +0530

    scroll problem solved

--- new.js
+++ new.js
@@ -17,7 +17,7 @@ init = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
-
+  renderer.setClearColor(0xd2c8c8, 1);
   var light = new THREE.AmbientLight(0x404040); // soft white light
   scene.add(light);
 
@@ -60,20 +60,37 @@ windowResize = () => {
 
 window.addEventListener("resize", windowResize, false);
 
-window.addEventListener("wheel", onMouseWheel, false);
+window.addEventListener("scroll", onMouseWheel, false);
+window.addEventListener(
+  "wheel",
+  (event) => {
+    dY = event.deltaY;
+    console.log("dY is-", dY);
+  },
+  false
+);
 
 animate = () => {
   requestAnimationFrame(animate);
-  scene.rotation.y += 0.01;
+  // cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 };
 
+// rotate = (event) => {
+//   setTimeout(() => {
+//     scene.rotation.y += event.deltaY * 0.0005;
+//   }, 0.001);
+// };
 function onMouseWheel(event) {
   event.preventDefault();
-  console.log("rotating ..");
-  document.getElementById("testing").style.color = "red";
+  console.log("rotating ..", event.deltaY);
 
+  document.getElementById("testing").style.color = "red";
+  setTimeout(() => {
+    scene.rotation.y += dY * 0.005;
+  }, 0.001);
+  // rotate(event);
   //   cube.rotation.x += event.deltaY * 0.0005;
 }
 
